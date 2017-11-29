49b054f734f861619a02c3b2fbce4b1568706a19
DrakDim <darsten.d@gmail.com>
2017-11-29 20:23:36 +0300

    Правка с вычислением исходной точки построения столбцов гистаграммы и общее улучшение именования переменных и констант

--- js/stat.js
+++ js/stat.js
@@ -1,27 +1,27 @@
 'use strict';
 window.renderStatistics = function (ctx, names, times) {
 
-  var INITIAL_STATICTICS_X = 100;
-  var INITIAL_STATICTICS_Y = 10;
+  var INITIAL_STAT_X = 100;
+  var INITIAL_STAT_Y = 10;
 
-  var OFFSET_SHADOW_STATISTIC_X = 10;
-  var OFFSET_SHADOW_STATISTIC_Y = 10;
+  var OFFSET_SHADOW_STAT_X = 10;
+  var OFFSET_SHADOW_STAT_Y = 10;
 
-  var SHADOW_X = INITIAL_STATICTICS_X + OFFSET_SHADOW_STATISTIC_X;
-  var SHADOW_Y = INITIAL_STATICTICS_Y + OFFSET_SHADOW_STATISTIC_Y;
+  var SHADOW_X = INITIAL_STAT_X + OFFSET_SHADOW_STAT_X;
+  var SHADOW_Y = INITIAL_STAT_Y + OFFSET_SHADOW_STAT_Y;
 
   var INITIAL_COLUMN_Y = 250;
   var INITIAL_COLUMN_X = 150;
 
-  var STATICTICS_WIDTH = 420;
-  var STATICTICS_HEIGHT = 270;
+  var STAT_WIDTH = 420;
+  var STAT_HEIGHT = 270;
 
   var HISTOGRAM_HEIGHT = 150;
   var HISTOGRAM_COLUMN_WIDTH = 40;
   var DISTANCE_BETWEEN_COLUMNS = 50;
 
-  var STATICTICS_COLOR = 'white';
-  var SHADOW_STATISTIC_COLOR = 'rgba(0, 0, 0, 0.7)';
+  var STAT_COLOR = 'white';
+  var SHADOW_STAT_COLOR = 'rgba(0, 0, 0, 0.7)';
   var DEFAULT_TEXT_COLOR = '#000';
   var FONT = '16px PT Mono';
   var WIN_PLAYER_COLOR = 'rgba(255, 0, 0, 1)';
@@ -60,25 +60,29 @@ window.renderStatistics = function (ctx, names, times) {
   };
 
   var drawHistogram = function (arrayTimes, arrayNames) {
+    var step = HISTOGRAM_HEIGHT / (searchMaxValue(arrayTimes) - 0);
     var columnHeight;
     var columnTextHeight;
     var playerColumnColor;
+    var columnY;
     var columnX;
     for (var i = 0; i < arrayTimes.length; i++) {
-      columnHeight = arrayTimes[i] * (HISTOGRAM_HEIGHT / (searchMaxValue(arrayTimes) - 0));
-      columnTextHeight = STATICTICS_HEIGHT - (columnHeight + 30);
+      columnHeight = arrayTimes[i] * step;
+      columnTextHeight = STAT_HEIGHT - (columnHeight + 30);
       playerColumnColor = arrayNames[i] === 'Вы' ? WIN_PLAYER_COLOR : 'rgba(0, 26, 255, ' + randomAlpha() + ')';
+
+      columnY = INITIAL_COLUMN_Y - columnHeight;
       columnX = INITIAL_COLUMN_X + i * (DISTANCE_BETWEEN_COLUMNS + HISTOGRAM_COLUMN_WIDTH);
 
-      drawColumn(columnX, INITIAL_COLUMN_Y, HISTOGRAM_COLUMN_WIDTH, -columnHeight, playerColumnColor);
+      drawColumn(columnX, columnY, HISTOGRAM_COLUMN_WIDTH, columnHeight, playerColumnColor);
 
       createText(arrayNames[i], columnX, INITIAL_COLUMN_Y + 20, DEFAULT_TEXT_COLOR);
       createText(Math.round(arrayTimes[i]), columnX, columnTextHeight, DEFAULT_TEXT_COLOR);
     }
   };
 
-  createRect(SHADOW_X, SHADOW_Y, STATICTICS_WIDTH, STATICTICS_HEIGHT, SHADOW_STATISTIC_COLOR);
-  createRect(INITIAL_STATICTICS_X, INITIAL_STATICTICS_Y, STATICTICS_WIDTH, STATICTICS_HEIGHT, STATICTICS_COLOR);
+  createRect(SHADOW_X, SHADOW_Y, STAT_WIDTH, STAT_HEIGHT, SHADOW_STAT_COLOR);
+  createRect(INITIAL_STAT_X, INITIAL_STAT_Y, STAT_WIDTH, STAT_HEIGHT, STAT_COLOR);
 
   createText('Ура вы победили!', 230, 40, DEFAULT_TEXT_COLOR);
   createText('Список результатов:', 220, 60, DEFAULT_TEXT_COLOR);
