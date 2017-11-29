2020fb08b6fc08e2f6360214a949357b82bc8c37
DrakDim <darsten.d@gmail.com>
2017-11-29 18:33:49 +0300

    Исправление в именовании констант

--- js/stat.js
+++ js/stat.js
@@ -1,26 +1,29 @@
 'use strict';
 window.renderStatistics = function (ctx, names, times) {
 
-  var INITIAL_WINDOW_STATICTICS_X = 100;
-  var INITIAL_WINDOW_STATICTICS_Y = 10;
+  var INITIAL_STATICTICS_X = 100;
+  var INITIAL_STATICTICS_Y = 10;
 
-  var OFFSET_SHADOW_WINDOW_STATISTIC_X = 10;
-  var OFFSET_SHADOW_WINDOW_STATISTIC_Y = 10;
+  var OFFSET_SHADOW_STATISTIC_X = 10;
+  var OFFSET_SHADOW_STATISTIC_Y = 10;
 
-  var INITIAL_COLUM_Y = 250;
+  var SHADOW_X = INITIAL_STATICTICS_X + OFFSET_SHADOW_STATISTIC_X;
+  var SHADOW_Y = INITIAL_STATICTICS_Y + OFFSET_SHADOW_STATISTIC_Y;
+
+  var INITIAL_COLUMN_Y = 250;
 
   var COLUMN_X = 150;
 
-  var WINDOW_STATICTICS_WIDTH = 420;
-  var WINDOW_STATICTICS_HEIGHT = 270;
+  var STATICTICS_WIDTH = 420;
+  var STATICTICS_HEIGHT = 270;
 
-  var HISTOGRAM_WINDOW_HEIGHT = 150;
+  var HISTOGRAM_HEIGHT = 150;
   var HISTOGRAM_COLUMN_WIDTH = 40;
   var DISTANCE_BETWEEN_COLUMNS = 50;
 
-  var WINDOW_STATICTICS_COLOR = 'white';
-  var SHADOW_WINDOW_STATISTIC_COLOR = 'rgba(0, 0, 0, 0.7)';
-  var CONGRATULATION_TEXT_COLOR = '#000';
+  var STATICTICS_COLOR = 'white';
+  var SHADOW_STATISTIC_COLOR = 'rgba(0, 0, 0, 0.7)';
+  var DEFAULT_TEXT_COLOR = '#000';
   var FONT = '16px PT Mono';
   var WIN_PLAYER_COLOR = 'rgba(255, 0, 0, 1)';
 
@@ -57,7 +60,7 @@ window.renderStatistics = function (ctx, names, times) {
   };
 
   var drawHistogram = function (arrayTimes, arrayNames) {
-    var step = HISTOGRAM_WINDOW_HEIGHT / (searchMaxValue(arrayTimes) - 0);
+    var step = HISTOGRAM_HEIGHT / (searchMaxValue(arrayTimes) - 0);
     var up;
     var playerColumnColor;
     var columnX;
@@ -66,19 +69,17 @@ window.renderStatistics = function (ctx, names, times) {
       playerColumnColor = arrayNames[i] === 'Вы' ? WIN_PLAYER_COLOR : 'rgba(0, 26, 255, ' + randomAlpha() + ')';
       columnX = COLUMN_X + i * (DISTANCE_BETWEEN_COLUMNS + HISTOGRAM_COLUMN_WIDTH);
 
-      createText(arrayNames[i], columnX, INITIAL_COLUM_Y + 20, CONGRATULATION_TEXT_COLOR);
+      createText(arrayNames[i], columnX, INITIAL_COLUMN_Y + 20, DEFAULT_TEXT_COLOR);
       drawColumn(columnX, 250, HISTOGRAM_COLUMN_WIDTH, -up, playerColumnColor);
-      createText(arrayTimes[i], columnX, 90, CONGRATULATION_TEXT_COLOR);
+      createText(arrayTimes[i], columnX, 90, DEFAULT_TEXT_COLOR);
     }
   };
 
-  var shadowX = INITIAL_WINDOW_STATICTICS_X + OFFSET_SHADOW_WINDOW_STATISTIC_X;
-  var shadowY = INITIAL_WINDOW_STATICTICS_Y + OFFSET_SHADOW_WINDOW_STATISTIC_Y;
-  createRect(shadowX, shadowY, WINDOW_STATICTICS_WIDTH, WINDOW_STATICTICS_HEIGHT, SHADOW_WINDOW_STATISTIC_COLOR);
-  createRect(INITIAL_WINDOW_STATICTICS_X, INITIAL_WINDOW_STATICTICS_Y, WINDOW_STATICTICS_WIDTH, WINDOW_STATICTICS_HEIGHT, WINDOW_STATICTICS_COLOR);
+  createRect(SHADOW_X, SHADOW_Y, STATICTICS_WIDTH, STATICTICS_HEIGHT, SHADOW_STATISTIC_COLOR);
+  createRect(INITIAL_STATICTICS_X, INITIAL_STATICTICS_Y, STATICTICS_WIDTH, STATICTICS_HEIGHT, STATICTICS_COLOR);
 
-  createText('Ура вы победили!', 230, 40, CONGRATULATION_TEXT_COLOR);
-  createText('Список результатов:', 220, 60, CONGRATULATION_TEXT_COLOR);
+  createText('Ура вы победили!', 230, 40, DEFAULT_TEXT_COLOR);
+  createText('Список результатов:', 220, 60, DEFAULT_TEXT_COLOR);
 
   drawHistogram(times, names);
 };
